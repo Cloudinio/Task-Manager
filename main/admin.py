58aa9c64fc08ee6f67761a5bd1257819fc7b38e7
Cloudinio <senya.popruga@mail.ru>
2025-11-12 15:41:00 +0300

    list_tasks

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Task
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ["name"]
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "category", "deadline", "created_at", "user", )
+    list_filter = ("status", "category")
+    search_fields = ["title", "description"]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
+from django.views.generic import ListView
+from main.models import Task
 
 
-def index(request):
-    return HttpResponse("Привет, это мой первый Django-проект!")
+def Hello(request):
+    return HttpResponse("Привет, это мое дз :)")
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "main/task_list.html"
+    context_object_name = "tasks"
+
+    def get_queryset(self):
+        qs = (Task.objects
+                    .select_related("category", "user")
+                    .order_by(*Task._meta.ordering))
+        
+        #если в ручке передается status, categort_id, то фильтрация по ним
+        status = self.request.GET.get("status")
+        category_id = self.request.GET.get("category")
+        if status:
+            qs = qs.filter(status=status)
+        if category_id:
+            qs = qs.filter(category_id=category_id)
+        return qs
